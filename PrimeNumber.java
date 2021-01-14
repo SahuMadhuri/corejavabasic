78df63a98532d50569537096aa3875b58bf5e64a
SahuMadhuri <madhurisahu410@gmail.com>
2021-01-14 16:47:20 +0530

    initial value

---
+++ PrimeNumber.java
@@ -0,0 +1,22 @@
+package in.basic.program;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int n=6,t=0;
+		for(int i = 0;i<=n;i++)
+		{
+		t=i;
+		}
+		if(n/t==0)
+		{
+		System.out.print(n+" is a prime Number.");
+		}
+		else{
+		System.out.print(n+" is not a prime Number.");
+		}
+		
+
+	}
+
+}
