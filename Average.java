672bfd0c2e92eda4b1c5d2520890791a4866a780
SahuMadhuri <madhurisahu410@gmail.com>
2021-01-14 16:11:43 +0530

    corejava

---
+++ Average.java
@@ -0,0 +1,18 @@
+package in.basic.program;
+
+public class Average {
+	public static void main(String[] args) {
+		int n,j=6;
+		n=5;
+		n=n-2;
+		
+		double k=0;
+		for(int i=1;i<=j;i++){
+			n=n+2;
+			k=k+2;
+		}
+		k=k/j;
+		System.out.println("The average of "+j+" consecutive even or odd no. is :"+k);
+	}
+
+}
